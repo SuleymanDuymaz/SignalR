fdf2bac3d8d1fc3e61cc2f32a82e937e33f02d2b
Süleyman <sduymaz8@gmail.com>
2023-04-08 23:00:29 +0300

    First commit of project

---
+++ SıgnalRServerExample/SıgnalRServerExample/Hubs/MyHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SıgnalRServerExample.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+          await  Clients.All.SendAsync("receiveMessage",message);
+        }   
+    }
+}
